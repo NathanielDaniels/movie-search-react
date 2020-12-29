8e3119c918c6a9f9ceb40e38567fb998ccf4536f
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-12-29 10:44:18 -0600

    search result poster images working

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -9,15 +9,12 @@ export default function SearchMovies(){
     
     const searchMovies = async (e) => {
         e.preventDefault();
-
-        // const query = "Jurassic Park"
                 
-        const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=5552fffb8a95ca814aa5e4b9d3a3c335&language=en-US&query=${query}&page=1&include_adult=false`;
         
         try {
             const res = await fetch(url);
             const data  = await res.json();
-            // console.log("data", data); 
             console.log(data.results)
             setMovies(data.results)
         }catch(err){
@@ -38,7 +35,8 @@ export default function SearchMovies(){
         <div className="card-list">
             {movies.map(movie => (
               <div className="card" key={movie.id}>
-                <img src={movie.poster_path} alt={movie.title}/>
+                {/* <img className="card--image" src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`} alt={movie.title}/> */}
+                <img className="card--image" src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title}/>
               </div>
             ))}
         </div>    
