a20fe8e0b3f36b6e85162c1f7c64ba1356649c1f
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-12-29 10:35:37 -0600

    adding search feature with movie title + img

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -5,24 +5,28 @@ export default function SearchMovies(){
     //states- input query, movies
     const [query, setQuery] = useState('');
     //create the state for movies, and update that sts
+    const [movies, setMovies] = useState([]);
     
     const searchMovies = async (e) => {
         e.preventDefault();
 
-        const query = "Jurassic Park"
+        // const query = "Jurassic Park"
                 
         const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
         
         try {
             const res = await fetch(url);
             const data  = await res.json();
-            console.log(data);
+            // console.log("data", data); 
+            console.log(data.results)
+            setMovies(data.results)
         }catch(err){
             console.error(err);
         } 
     }
     
     return (
+      <>
         <form className="form" onSubmit={searchMovies}>
             <label className="label" htmlFor="query">Movie Name</label>
             <input className="input" type="text" name="query"
@@ -31,5 +35,13 @@ export default function SearchMovies(){
                 />
             <button className="button" type="submit">Search</button>
         </form>
+        <div className="card-list">
+            {movies.map(movie => (
+              <div className="card" key={movie.id}>
+                <img src={movie.poster_path} alt={movie.title}/>
+              </div>
+            ))}
+        </div>    
+      </>
     )
 }
