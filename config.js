5a522c50c89792f9cc8d657cd0f51ce0cb3c4c65
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-12-29 11:05:39 -0600

    added webpack

---
+++ config.js
@@ -0,0 +1,3 @@
+let config = {
+  MY_KEY : '5552fffb8a95ca814aa5e4b9d3a3c335',
+}
--- src/searchMovies.js
+++ src/searchMovies.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import config from '../config'
 
 export default function SearchMovies(){
     
@@ -9,6 +10,8 @@ export default function SearchMovies(){
     
     const searchMovies = async (e) => {
         e.preventDefault();
+
+        const mykey = config.MY_KEY;
                 
         const url = `https://api.themoviedb.org/3/search/movie?api_key=5552fffb8a95ca814aa5e4b9d3a3c335&language=en-US&query=${query}&page=1&include_adult=false`;
         
@@ -35,8 +38,8 @@ export default function SearchMovies(){
         <div className="card-list">
             {movies.map(movie => (
               <div className="card" key={movie.id}>
-                {/* <img className="card--image" src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`} alt={movie.title}/> */}
-                <img className="card--image" src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title}/>
+                <img className="card--image" src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`} alt={movie.title}/>
+                <span>{movie.title}</span>
               </div>
             ))}
         </div>    
